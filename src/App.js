43abc4cb0ac199f178a6034992567d34c3b2cba8
Jaden Ussher <jaden.ussher@connect.qut.edu.au>
2024-11-17 21:34:51 +1000

    added fonts

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <img src="/images/mesh-gradient (1).png" alt="Description of the image" />
-      
+
+      <section id="main">
+        <img src="/images/mesh-gradient (1).png" alt="Description of the image" />
+        <h1>Welcome</h1>
+      </section> 
+
       <section id="about">
-        <h1>About Me</h1>
+        <h2>About Me</h2>
         <p>I am a fifth-year engineering and IT student...</p>
       </section>
       <section id="projects">
-        <h1>Projects</h1>
+        <h2>Projects</h2>
         <p>Here are some of the projects I have worked on...</p>
       </section>
       <section id="experience">
-        <h1>Experience</h1>
+        <h2>Experience</h2>
         <p>Details about my professional experience...</p>
       </section>
       <section id="contact">
-        <h1>Contact</h1>
+        <h2>Contact</h2>
         <p>You can reach me at example@example.com</p>
       </section>
     </div>
