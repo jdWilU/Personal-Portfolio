4c63526ae983b35208557c7e68f11c6e263f97f1
Jaden Ussher <jaden.ussher@connect.qut.edu.au>
2024-11-17 20:54:26 +1000

    navbar page linking

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import './App.css';
-import Navbar from './Navbar';
-
+import React from "react";
+import Navbar from "./Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar>
-
-      </Navbar>
+      <Navbar />
+      <section id="about" style={{ padding: "100px 20px", height: "100vh" }}>
+        <h1>About Me</h1>
+        <p>Details about yourself.</p>
+      </section>
+      <section id="projects" style={{ padding: "100px 20px", height: "100vh" }}>
+        <h1>Projects</h1>
+        <p>Details about your projects.</p>
+      </section>
+      <section id="experience" style={{ padding: "100px 20px", height: "100vh" }}>
+        <h1>Experience</h1>
+        <p>Details about your experience.</p>
+      </section>
+      <section id="contact" style={{ padding: "100px 20px", height: "100vh" }}>
+        <h1>Contact</h1>
+        <p>Contact details.</p>
+      </section>
     </div>
-    
   );
 }
 
