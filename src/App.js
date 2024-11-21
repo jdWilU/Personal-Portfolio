11599a7959ef195b7688064ffb6028afa2bd5cab
Jaden Ussher <jaden.ussher@connect.qut.edu.au>
2024-11-21 15:20:31 +1000

    update css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import "./App.css";
+import "./main.css"
 
 function App() {
   return (
@@ -23,14 +24,20 @@ function App() {
         <h2>About Me</h2>
         <p>I am a fifth-year engineering and IT student...</p>
       </section>
+
+
       <section id="projects">
         <h2>Projects</h2>
         <p>Here are some of the projects I have worked on...</p>
       </section>
+
+
       <section id="experience">
         <h2>Experience</h2>
         <p>Details about my professional experience...</p>
       </section>
+
+      
       <section id="contact">
         <h2>Contact</h2>
         <p>You can reach me at example@example.com</p>
