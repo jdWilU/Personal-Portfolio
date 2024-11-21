0aa06ea1a1237192d4555b5b86fc8874178021e9
Jaden Ussher <jaden.ussher@connect.qut.edu.au>
2024-11-21 15:08:16 +1000

    update home

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ function App() {
       <Navbar />
 
       <section id="main">
-        <img src="/images/mesh-gradient (1).png" alt="Description of the image" />
-        <h1>Welcome</h1>
-      </section> 
+        <img src="/images/mesh-gradient (1).png" alt="Background gradient" />
+        <div className="main-content">
+          <h1>HI, I'M JADEN.</h1>
+          <p>
+            I am a fifth-year engineering and information technology student,
+            majoring in aerospace and electrical engineering, and computer science.
+          </p>
+        </div>
+      </section>
+
 
       <section id="about">
         <h2>About Me</h2>
@@ -33,3 +40,4 @@ function App() {
 }
 
 export default App;
+
