0225eea98af3478017bb78db746603ab02101b28
Jaden Ussher <jaden.ussher@connect.qut.edu.au>
2025-03-05 16:27:16 +1000

    implemented containers

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ function App() {
   return (
     <div className="App">
       <div className='background-container'>
-          <div className='content-box'>
+        <div className='content-box'>
+          <div className='left-container'>
+            {/* Left content */}
+          </div>
+          <div className='right-container'> 
             <h1>USSHER, JADEN</h1>
+            <h2>AEROSPACE & ELECTRICAL ENGINEER + COMPUTER SCIENTIST</h2>
           </div>
         </div>
+      </div>
     </div>
   );
 }
