6d823fc51bdaa58f27af0b78e30c604eb024b118
Jaden Ussher <jaden.ussher@connect.qut.edu.au>
2025-03-05 15:30:59 +1000

    minor styling changes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='background-containter'>
-        <h1>Hello World</h1>
+      <div className='background-container'>
+          <div className='content-box'>
+            <h1>USSHER, JADEN</h1>
+          </div>
         </div>
     </div>
   );
